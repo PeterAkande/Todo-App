1bfc5999b5aaa7f50867ec13e650081132ab3544
PeterAkande <akandepeter86@gmail.com>
2022-10-28 19:31:42 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Todo App
+
+This would be a simple todo app
+Use Hive as DB and Bloc as for State Management
---
+++ android/app/src/main/kotlin/com/pilad/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.pilad.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
