e422ff2f2cad4c4a30e633399430fc06f4cb4f84
PeterAkande <akandepeter86@gmail.com>
2022-11-21 02:28:42 +0100

    Add the states the Todo bloc would emit

--- lib/blocs/todo/todo_state.dart
+++ lib/blocs/todo/todo_state.dart
@@ -1,10 +1,22 @@
 part of 'todo_bloc.dart';
 
+enum TodoStatus { loading, success, failure }
+
 class TodoState extends Equatable {
-  const TodoState(this.todos);
+  const TodoState({this.todos = const [], this.status = TodoStatus.loading});
 
   final List<TodoModel> todos;
+  final TodoStatus status;
+
+  TodoState copyWith(
+      {TodoStatus Function()? currentStatus,
+      List<TodoModel> Function()? allTodos}) {
+    return TodoState(
+        todos: allTodos != null ? allTodos() : todos,
+        status: currentStatus != null ? currentStatus() : status);
+  }
 
   @override
-  List get props => [todos]; //If todos is changes, a rebuild is triggered
+  List get props =>
+      [todos, status]; //If todos is changes, a rebuild is triggered
 }
