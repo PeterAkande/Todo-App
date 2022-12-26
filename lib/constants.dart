441ef0cf666c0a3f7c6d9469c74c67401aeac40b
PeterAkande <akandepeter86@gmail.com>
2022-12-26 11:08:34 +0100

    Add new constant colors

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,9 +2,8 @@
 import 'package:flutter/material.dart';
 
 const Color appPrimaryColorLight = Colors.white70;
-const Color appSecondaryColorLight = Colors.black54;
+const Color appSecondaryColorLight = Color(0xFF363636);
 
 const Color appPrimaryColorDark = Colors.black54;
 const Color appSecondaryColorDark = Colors.white70;
 
-const Color accentColor = Colors.yellowAccent;
