386e523152f282b7c4862907b386493c77a37ba8
PeterAkande <akandepeter86@gmail.com>
2022-12-27 11:09:04 +0100

    Update required parameters forthe todo app state

--- lib/routers/cubit/todo_app_state.dart
+++ lib/routers/cubit/todo_app_state.dart
@@ -1,8 +1,15 @@
 part of 'todo_app_cubit.dart';
 
 class TodoAppState extends Equatable {
-  TodoModel? selectedTodo; //This can be null.
-  TodoAppState({TodoModel? selectedTodo});
+  //The selectedTodo can be null. If it isn't null, the details screen would be shown. with the selected _todo
+  //details shown.
+  //If the addOrEditingATodo is true, it means that a todo is to be added or edited. If it is to be added.
+  //Or edited depends on the value of selectedTodo
+
+  TodoModel? selectedTodo;
+  bool addOrEditingATodo;
+
+  TodoAppState({TodoModel? selectedTodo, this.addOrEditingATodo = false});
 
   @override
   List<Object?> get props => [selectedTodo];
