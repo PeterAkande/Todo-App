60f967afcbefb44624d983b0217a3bdee0352f49
PeterAkande <akandepeter86@gmail.com>
2022-12-27 13:01:31 +0100

    Add: Add a line to act as a separator between views for desktop

---
+++ lib/components/desktop_view_line_separator.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DeskTopViewsSeparator extends StatelessWidget {
+  const DeskTopViewsSeparator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 0.5,
+      color: Colors.black,
+    );
+  }
+}
