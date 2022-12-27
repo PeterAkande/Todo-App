8876e4ceb6a67283ef293f991abdfffc9dbc8a23
PeterAkande <akandepeter86@gmail.com>
2022-12-27 11:11:45 +0100

    Add: Add additional states that the todo details bloc would emit

--- lib/screens/todo_details_screen/bloc/todo_details_state.dart
+++ lib/screens/todo_details_screen/bloc/todo_details_state.dart
@@ -2,9 +2,27 @@ part of 'todo_details_bloc.dart';
 
 abstract class TodoDetailsState extends Equatable {
   const TodoDetailsState();
-}
 
-class TodoDetailsInitial extends TodoDetailsState {
   @override
   List<Object> get props => [];
+}
+
+class TodoDetailsInitial extends TodoDetailsState {}
+
+class EditingTodoDetailsState extends TodoDetailsState {
+  final TodoModel todoModel;
+
+  const EditingTodoDetailsState(this.todoModel);
+//This is the details when a _todo details is being edited
+}
+
+class AddingNewTodoDetailsState extends TodoDetailsState {
+  //This is the stat when a new _todo is being created
+}
+
+class ViewingTodoDetailsState extends TodoDetailsState {
+  //This is the event that would be when a _todo details is being viewed
+  final TodoModel todoModel;
+
+  const ViewingTodoDetailsState(this.todoModel);
 }
