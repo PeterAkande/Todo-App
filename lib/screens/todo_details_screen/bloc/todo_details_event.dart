3ce97e1d40a75417f31fba55e42d7c9c15c3fc2a
PeterAkande <akandepeter86@gmail.com>
2022-12-27 11:11:24 +0100

    Add: Add additional events that the todo details bloc would respond to

--- lib/screens/todo_details_screen/bloc/todo_details_event.dart
+++ lib/screens/todo_details_screen/bloc/todo_details_event.dart
@@ -1,5 +1,25 @@
 part of 'todo_details_bloc.dart';
 
-abstract class TodoDetailsEvent extends Equatable {
+abstract class TodoDetailsEvent {
   const TodoDetailsEvent();
+}
+
+class TodoDetailsSubscriptionRequested extends TodoDetailsEvent {}
+
+class EditTodoDetailsRequested extends TodoDetailsEvent {
+  //This is the event that would be emitted when a _todo details is to edited
+
+  final TodoModel todo;
+  const EditTodoDetailsRequested(this.todo);
+}
+
+class AddNewTodoDetailsRequested extends TodoDetailsEvent {
+  //This is when a new _todo details is to be added.
+}
+
+class ViewTodoDetailsRequested extends TodoDetailsEvent {
+  //This is when the details of a _todo is to be viewed. No editing of the todo is allowed here.
+
+  final TodoModel todo;
+  const ViewTodoDetailsRequested(this.todo);
 }
