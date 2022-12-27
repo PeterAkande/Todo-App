1e5ea60ae6f9cef0446880bf126e87d751d645a3
PeterAkande <akandepeter86@gmail.com>
2022-12-27 11:12:14 +0100

    Update widget name and children

--- lib/screens/todo_list_home_screen/todo_list_home_screen.dart
+++ lib/screens/todo_list_home_screen/todo_list_home_screen.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:todo_app/constants.dart';
-import 'package:todo_app/screens/todo_details_screen/todo_details_screen.dart';
+import '../todo_details_screen/todo_details_screen.dart';
+import 'components/left_pane_for_desktop_view.dart';
 
-class TodoListHomeScreen extends StatelessWidget {
-  const TodoListHomeScreen({Key? key}) : super(key: key);
+class TodoListHomeScreenDesktop extends StatelessWidget {
+  const TodoListHomeScreenDesktop({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +12,7 @@ class TodoListHomeScreen extends StatelessWidget {
       child: Scaffold(
         backgroundColor: Colors.white,
         body: Row(
-          children: [
+          children: const [
             LeftPanelView(),
             Expanded(flex: 2, child: HomeScreenView()),
             Expanded(
@@ -35,42 +34,6 @@ class HomeScreenView extends StatelessWidget {
     return Container(
       width: 20,
       color: Colors.pink,
-    );
-  }
-}
-
-class LeftPanelView extends StatelessWidget {
-  const LeftPanelView({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.only(top: 30, bottom: 10),
-      width: 80,
-      color: Colors.red,
-      child: Column(
-        children: [
-          SvgPicture.asset('icons/filter_icon.svg'),
-          SizedBox(
-            height: 30,
-          ),
-          SvgPicture.asset('icons/options_icon.svg'),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                ElevatedButton(
-                  style: ButtonStyle(
-
-                  ),
-                  onPressed: () {},
-                  child: const Icon(Icons.add),
-                )
-              ],
-            ),
-          )
-        ],
-      ),
     );
   }
 }
