8d6f78a7c3eee28aa67b20e49c095429670231a2
PeterAkande <akandepeter86@gmail.com>
2023-01-02 04:48:22 +0100

    Update the UI of the screen

--- lib/screens/todo_list_home_screen/todo_list_home_screen.dart
+++ lib/screens/todo_list_home_screen/todo_list_home_screen.dart
@@ -4,7 +4,9 @@ import 'package:flutter_svg/svg.dart';
 import 'package:todo_repository/todo_repository.dart';
 
 import '../../responsive_info.dart';
+import '../../routers/cubit/todo_app_cubit.dart';
 import 'blocs/todo_bloc.dart';
+import 'components/empty_todos_view.dart';
 
 class HomeScreenView extends StatelessWidget {
   HomeScreenView({Key? key}) : super(key: key);
@@ -62,114 +64,132 @@ class HomeScreenView extends StatelessWidget {
                     height: 44,
                   ),
                   Expanded(
-                    child: ListView.separated(
-                        itemBuilder: (context, index) {
-                          TodoModel todoModel = state.todos[index];
-                          return ClipRRect(
-                            borderRadius: BorderRadius.circular(5),
-                            child: Container(
-                              color: const Color(0xFFD2D2D2).withOpacity(0.3),
-                              height: 90,
-                              child: Material(
-                                elevation: 0,
-                                color: Colors.transparent,
-                                child: InkWell(
-                                  onTap: () {},
-                                  child: Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        horizontal: 10, vertical: 7),
-                                    child: LayoutBuilder(
-                                        builder: (context, constraints) {
-                                      return Row(
-                                        children: [
-                                          SizedBox(
-                                            width: constraints.maxWidth * 0.75,
-                                            child: Column(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.center,
-                                              crossAxisAlignment:
-                                                  CrossAxisAlignment.start,
-                                              children: [
-                                                Text(
-                                                  todoModel.title,
-                                                  style: Theme.of(context)
-                                                      .textTheme
-                                                      .headline3
-                                                      ?.copyWith(
-                                                          fontSize: 20,
-                                                          fontWeight:
-                                                              FontWeight.bold),
-                                                  overflow:
-                                                      TextOverflow.ellipsis,
-                                                  maxLines: 1,
+                    child: state.todos.isEmpty
+                        ? const EmptyTodosView()
+                        : ListView.separated(
+                            itemBuilder: (context, index) {
+                              TodoModel todoModel = state.todos[index];
+                              return ClipRRect(
+                                borderRadius: BorderRadius.circular(5),
+                                child: Container(
+                                  color:
+                                      const Color(0xFFD2D2D2).withOpacity(0.3),
+                                  height: 90,
+                                  child: Material(
+                                    elevation: 0,
+                                    color: Colors.transparent,
+                                    child: InkWell(
+                                      onTap: () {
+                                        context
+                                            .read<TodoAppCubit>()
+                                            .selectTodo(todoModel);
+                                      },
+                                      child: Padding(
+                                        padding: const EdgeInsets.symmetric(
+                                            horizontal: 10, vertical: 7),
+                                        child: LayoutBuilder(
+                                            builder: (context, constraints) {
+                                          return Row(
+                                            children: [
+                                              SizedBox(
+                                                width:
+                                                    constraints.maxWidth * 0.75,
+                                                child: Column(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.center,
+                                                  crossAxisAlignment:
+                                                      CrossAxisAlignment.start,
+                                                  children: [
+                                                    Text(
+                                                      todoModel.title,
+                                                      style: Theme.of(context)
+                                                          .textTheme
+                                                          .headline3
+                                                          ?.copyWith(
+                                                              fontSize: 20,
+                                                              fontWeight:
+                                                                  FontWeight
+                                                                      .bold),
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                      maxLines: 1,
+                                                    ),
+                                                    const SizedBox(
+                                                      height: 10,
+                                                    ),
+                                                    Text(
+                                                      todoModel
+                                                          .additionalContents,
+                                                      style: Theme.of(context)
+                                                          .textTheme
+                                                          .headline4
+                                                          ?.copyWith(
+                                                            fontSize: 15,
+                                                          ),
+                                                      maxLines: 1,
+                                                      overflow:
+                                                          TextOverflow.ellipsis,
+                                                    )
+                                                  ],
                                                 ),
-                                                const SizedBox(
-                                                  height: 10,
-                                                ),
-                                                Text(
-                                                  todoModel.additionalContents,
-                                                  style: Theme.of(context)
-                                                      .textTheme
-                                                      .headline4
-                                                      ?.copyWith(
-                                                        fontSize: 15,
-                                                      ),
-                                                )
-                                              ],
-                                            ),
-                                          ),
-                                          Expanded(
-                                            child: Row(
-                                              mainAxisAlignment:
-                                                  MainAxisAlignment.end,
-                                              children: [
-                                                IconButton(
-                                                  iconSize: 25,
-                                                  onPressed: () {},
-                                                  icon: const Icon(
-                                                      Icons.edit_outlined),
-                                                ),
-                                                Checkbox(
-                                                    side: const BorderSide(
-                                                        color: Colors.black),
-                                                    shape:
-                                                        RoundedRectangleBorder(
+                                              ),
+                                              Expanded(
+                                                child: Row(
+                                                  mainAxisAlignment:
+                                                      MainAxisAlignment.end,
+                                                  children: [
+                                                    IconButton(
+                                                      iconSize: 25,
+                                                      onPressed: () {},
+                                                      icon: const Icon(
+                                                          Icons.edit_outlined),
+                                                    ),
+                                                    Checkbox(
+                                                        side: const BorderSide(
+                                                            color:
+                                                                Colors.black),
+                                                        shape: RoundedRectangleBorder(
                                                             borderRadius:
                                                                 BorderRadius
                                                                     .circular(
                                                                         2)),
-                                                    fillColor:
-                                                        const MaterialStatePropertyAll(
-                                                      Colors.transparent,
-                                                    ),
-                                                    checkColor: Colors.black,
-                                                    value: todoModel.completed,
-                                                    onChanged: (bool? value) {})
-                                              ],
-                                            ),
-                                          )
-                                        ],
-                                      );
-                                    }),
+                                                        fillColor:
+                                                            const MaterialStatePropertyAll(
+                                                          Colors.transparent,
+                                                        ),
+                                                        checkColor:
+                                                            Colors.black,
+                                                        value:
+                                                            todoModel.completed,
+                                                        onChanged:
+                                                            (bool? value) {})
+                                                  ],
+                                                ),
+                                              )
+                                            ],
+                                          );
+                                        }),
+                                      ),
+                                    ),
                                   ),
                                 ),
-                              ),
-                            ),
-                          );
-                        },
-                        separatorBuilder: (context, index) {
-                          return const SizedBox(
-                            height: 10,
-                          );
-                        },
-                        itemCount: state.todos.length),
+                              );
+                            },
+                            separatorBuilder: (context, index) {
+                              return const SizedBox(
+                                height: 10,
+                              );
+                            },
+                            itemCount: state.todos.length),
                   )
                 ],
               ),
             ),
             floatingActionButton: responsiveInfoRepo.isMobile
                 ? FloatingActionButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      context.read<TodoAppCubit>().createNewTodo();
+                    },
                     child: const Icon(Icons.add),
                   )
                 : null);
